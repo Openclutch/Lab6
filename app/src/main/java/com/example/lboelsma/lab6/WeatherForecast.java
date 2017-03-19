c542e844f8994c2247f1cf9150c1529e3e0366b6
lee <leelandboelsma@gmail.com>
2017-03-19 17:16:01 -0400

    Added the rest of the code so it is complete

--- app/src/main/java/com/example/lboelsma/lab6/WeatherForecast.java
+++ app/src/main/java/com/example/lboelsma/lab6/WeatherForecast.java
@@ -1,12 +1,19 @@
 package com.example.lboelsma.lab6;
 
 import android.app.Activity;
+import android.content.Loader;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.util.Log;
 import android.util.Xml;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
@@ -27,84 +34,160 @@ public class WeatherForecast extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather_forecast);
 
-        // make the progress bar visible onCreate()
-        ProgressBar fetchProgress = (ProgressBar) (findViewById(R.id.progressBar));
-        fetchProgress.setVisibility(View.VISIBLE);
+        AsyncTask task = new AsyncTask() {
 
+            private String tempMin;
+            private String tempMax;
+            private String tempCurrent;
+            private String weatherIcon;
+            private Bitmap weatherIconBitmap;
 
-    }
+            @Override
+            protected Object doInBackground(Object ...args) {
 
-    private class ForecastQuery extends AsyncTask<String, Integer, String> {
+                try {
+                    URL url = new URL("http://api.openweathermap.org/data/2.5/weather?q=ottawa,ca&" +
+                            "APPID=d99666875e0e51521f0040a3d97d0f6a&mode=xml&units=metric");
 
-        private String tempMin;
-        private String tempMax;
-        private String tempCurrent;
-        private Bitmap bitmap;
+                    // Create connection object
+                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
-        @Override
-        protected String doInBackground(String ...args) {
-            String url = ("http://api.openweathermap.org/data/2.5/weather?q=ottawa,ca&" +
-                    "APPID=d99666875e0e51521f0040a3d97d0f6a&mode=xml&units=metric");
+                    // Settings for connection
+                    connection.setReadTimeout(10000);
+                    connection.setConnectTimeout(15000);
+                    connection.setRequestMethod("GET");
+                    connection.connect();
 
-            try {
-                parse(downloadUrl(url));
-            } catch(XmlPullParserException xml) {
-                xml.printStackTrace();
-            } catch(IOException io) {
-                io.printStackTrace();
-            } catch(Exception e) {
-                e.printStackTrace();
-            }
+                    // Actually downloads the data into "in"
+                    InputStream in = connection.getInputStream();
 
-            return null;
-        }
+                    // Create the Xml parser object
+                    XmlPullParser parser = Xml.newPullParser();
+                    parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
 
-        // Given a string representation of a URL, sets up a connection and gets
-        // an input stream.
-        private InputStream downloadUrl(String urlString) throws IOException {
-            URL url = new URL(urlString);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setReadTimeout(10000 /* milliseconds */);
-            conn.setConnectTimeout(15000 /* milliseconds */);
-            conn.setRequestMethod("GET");
-            conn.setDoInput(true);
-            // Starts the query
-            conn.connect();
-            return conn.getInputStream();
-        }
+                    // Put the connection that has been downloaded into the xml parser
+                    parser.setInput(in, null);
 
-        // parse the downloadUrl
-        public List parse(InputStream in) throws XmlPullParserException, IOException {
-            try {
-                XmlPullParser parser = Xml.newPullParser();
-                parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
-                parser.setInput(in, null);
-                parser.nextTag();
-                return readFeed(parser);
-            } finally {
-                in.close();
-            }
-        }
+                    // Tell the parser to go to the next tag, maybe if statement here?
+                    parser.nextTag();
 
-        private List readFeed(XmlPullParser parser) throws XmlPullParserException, IOException {
-            List entries = new ArrayList();
+                    int eventType = parser.getEventType();
 
-            parser.require(XmlPullParser.START_TAG, null, "feed");
-            while (parser.next() != XmlPullParser.END_TAG) {
-                if (parser.getEventType() != XmlPullParser.START_TAG) {
-                    continue;
+                    while (eventType != XmlPullParser.END_DOCUMENT) {
+
+                        if (eventType == XmlPullParser.START_TAG) {
+
+                            String name = parser.getName();
+
+                            // <temperature value="-7.02" min="-8" max="-6" unit="metric"/>
+                            if (name.equalsIgnoreCase("temperature")) {
+
+                                int n = parser.getAttributeCount();
+                                for (int i = 0; i < n; i++) {
+
+                                    String attr = parser.getAttributeName(i);
+                                    if (attr.equalsIgnoreCase("value")) {
+                                        this.tempCurrent = parser.getAttributeValue(i);
+                                        publishProgress(25);
+                                        Log.i("testing", "Temperature current:" + this.tempCurrent);
+                                    } else if (attr.equalsIgnoreCase("min")) {
+                                        this.tempMin = parser.getAttributeValue(i);
+                                        publishProgress(50);
+                                        Log.i("testing", "Temperature min:" + this.tempMin);
+                                    } else if (attr.equalsIgnoreCase("max")) {
+                                        this.tempMax = parser.getAttributeValue(i);
+                                        publishProgress(75);
+                                        Log.i("testing", "Temperature max:" + this.tempMax);
+                                    }
+                                }
+                            }
+                            //<weather number="800" value="clear sky" icon="02d"/>
+                            else if (name.equalsIgnoreCase("weather")) {
+                                int n = parser.getAttributeCount();
+                                for (int i = 0; i < n; i++) {
+
+                                    String attr = parser.getAttributeName(i);
+                                    if (attr.equalsIgnoreCase("icon")) {
+                                        this.weatherIcon = parser.getAttributeValue(i);
+                                        Log.i("testing", "icon type:" + this.weatherIcon);
+                                    }
+                                }
+                            }
+                        }
+                        eventType = parser.next();
+                    }
+                } catch (Exception e) {
+                    return e;
                 }
-                String name = parser.getName();
-                // Starts by looking for the entry tag
-                if (name.equals("entry")) {
-                    entries.add(readEntry(parser));
-                } else {
-                    skip(parser);
+
+                try {
+                    URL url = new URL("http://openweathermap.org/img/w/" + this.weatherIcon + ".png");
+
+                    // Create connection object
+                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+                    // Settings for connection
+                    connection.connect();
+
+                    // Downloads the picture
+                    this.weatherIconBitmap = BitmapFactory.decodeStream(connection.getInputStream());
+
+                    publishProgress(100);
+
+                } catch (Exception e) {
+                    return e;
                 }
+
+                // I'm a noob
+                return null;
             }
-            return entries;
-        }
 
+            @Override
+            protected void onProgressUpdate(Object... value){
 
+                //set the visibility of the progress bar to visible
+                ProgressBar fetchProgress = (ProgressBar) (findViewById(R.id.progressBar));
+                fetchProgress.setVisibility(View.VISIBLE);
+
+                Log.i("testing", "Inside onProgressUpdate.");
+
+                //set the progressBar progress from the "value" being passed to this
+                int v = (Integer) value[0];
+                fetchProgress.setProgress(v);
+
+            }
+
+            @Override
+            protected void onPostExecute(Object result) {
+
+                // Set min text
+                TextView textTempMin = (TextView) (findViewById(R.id.textView_minTemp));
+                textTempMin.setText("Min: " + this.tempMin);
+                Log.i("testing", "setting the min temp as: " + this.tempMin);
+
+                // Set max text
+                TextView textTempMax = (TextView) (findViewById(R.id.textView_maxTemp));
+                textTempMax.setText("Max: " + this.tempMax);
+                Log.i("testing", "setting the max temp as: " + this.tempMax);
+
+                // Set curr text
+                TextView textTempCurr = (TextView) (findViewById(R.id.textView_currTemp));
+                textTempCurr.setText("Curr: " + this.tempCurrent);
+                Log.i("testing", "setting the current temp as: " + this.tempCurrent);
+
+                //update picture for the weather
+                ImageView weatherImage = (ImageView) (findViewById(R.id.imageView));
+                weatherImage.setImageBitmap(this.weatherIconBitmap);
+                Log.i("testing", "setting the weather icon");
+
+                //set visibility of the progress bar to invisible
+                ProgressBar fetchProgress = (ProgressBar) (findViewById(R.id.progressBar));
+                fetchProgress.setVisibility(View.INVISIBLE);
+            }
+
+        };
+
+        // execute launches a new thread
+        task.execute();
     }
 }
